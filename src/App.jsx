71bc58cb94ed6d06f8d6a5f61b598f04d1a17851
Pranjul Agrawal <pranjulagrawal9@gmail.com>
2024-03-05 20:48:56 +0530

    Implement Random Keypad numbers logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,87 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import { IoIosBackspace } from "react-icons/io";
 import { FaExclamationCircle } from "react-icons/fa";
 
 function App() {
+  const [keyPadNumbers, setKeyPadNumbers] = useState([
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+  ]);
   const [pin, setPin] = useState(new Array(4).fill(""));
-  const currPinLength = pin.join("").length;
+  const [totalDigits, setTotalDigits] = useState(4);
 
-  function handleNumberPress(key) {
-    if (currPinLength >= 4) return;
-
-    const currPin = [...pin];
-    currPin[currPinLength] = key;
-    setPin(currPin);
-  }
-
-  function handleSubmit() {
-    if (currPinLength === 4) console.log(pin);
-  }
-
-  function handleBackspace() {
-    if (currPinLength > 0) {
+  const handleNumberPress = useCallback(
+    function handleNumberPress(key) {
+      if (pin.join("").length >= totalDigits) return;
       const currPin = [...pin];
-      currPin[currPinLength - 1] = "";
+      currPin[pin.join("").length] = Number(key);
       setPin(currPin);
+    },
+    [pin, totalDigits]
+  );
+
+  const handleSubmit = useCallback(
+    function handleSubmit() {
+      if (pin.join("").length === totalDigits) console.log(pin);
+    },
+    [pin, totalDigits]
+  );
+
+  const handleBackspace = useCallback(
+    function handleBackspace() {
+      if (pin.join("").length > 0) {
+        const currPin = [...pin];
+        currPin[pin.join("").length - 1] = "";
+        setPin(currPin);
+      }
+    },
+    [pin]
+  );
+
+  function handleInputBox(e) {
+    const digits = Number(e.target.value);
+    if (digits > 8) return;
+    setTotalDigits(digits);
+  }
+
+  function handleEnter(e) {
+    e.stopPropagation();
+    if (e.key === "Enter") {
+      setPin(new Array(totalDigits).fill(""));
     }
   }
 
+  function handleRandomize() {
+    const newKeypadArray = [...keyPadNumbers];
+    newKeypadArray.sort(() => Math.random() - 0.5);
+    setKeyPadNumbers(newKeypadArray);
+  }
+
+  useEffect(() => {
+    function handleKeyboard(e) {
+      if (!isNaN(e.key)) {
+        handleNumberPress(e.key);
+      } else if (e.key === "Backspace") {
+        handleBackspace();
+      } else if (e.key === "Enter") {
+        handleSubmit();
+      }
+    }
+
+    document.addEventListener("keydown", handleKeyboard);
+
+    return () => document.removeEventListener("keydown", handleKeyboard);
+  }, [handleNumberPress, handleBackspace, handleSubmit]);
+
   return (
-    <div className="flex justify-center">
+    <div className="flex flex-col items-center">
+      <input
+        type="text"
+        className="border-2"
+        value={totalDigits}
+        onChange={handleInputBox}
+        onKeyDown={handleEnter}
+      />
       <div className="w-96 mt-10 border-2 p-5">
         <div className="bg-gray-200 flex justify-between p-2 mb-20 font-medium">
           <div>
@@ -44,16 +97,14 @@ function App() {
         <div className="flex flex-col items-center gap-3">
           <h2 className="uppercase font-medium">Enter 4-digit upi pin</h2>
           <div className="flex gap-4">
-            {Array(4)
-              .fill()
-              .map((_, i) => (
-                <div
-                  key={i}
-                  className={`w-4 h-4 border border-black rounded-full ${
-                    i + 1 <= currPinLength ? "bg-black" : "bg-white"
-                  }`}
-                ></div>
-              ))}
+            {pin.map((_, i) => (
+              <div
+                key={i}
+                className={`w-4 h-4 border border-black rounded-full ${
+                  i + 1 <= pin.join("").length ? "bg-black" : "bg-white"
+                }`}
+              ></div>
+            ))}
           </div>
         </div>
 
@@ -63,30 +114,32 @@ function App() {
           <p>You are sending ₹ 500.00 from your account to SANKALP GULATI</p>
         </div>
 
-        <div className="grid grid-cols-3 mt-24 text-lg">
-          {Array(9)
-            .fill()
-            .map((_, i) => (
-              <button
-                key={i + 1}
-                className="border text-center py-2 font-medium"
-                onClick={() => handleNumberPress(i + 1)}
-              >
-                {i + 1}
-              </button>
-            ))}
+        <button
+          className="bg-gray-200 p-2 rounded-md ml-5 mt-10"
+          onClick={handleRandomize}
+        >
+          Randomize Keyboard
+        </button>
+
+        <div className="grid grid-cols-3 mt-10 text-lg">
+          {keyPadNumbers.map((value, i) => (
+            <button
+              key={i}
+              className={`border text-center py-2 font-medium ${
+                i === 0 ? "col-start-2 col-end-3 row-start-4" : ""
+              }`}
+              onClick={() => handleNumberPress(i)}
+            >
+              {value}
+            </button>
+          ))}
           <button
-            className="border flex justify-center py-2"
+            className="border flex justify-center py-2 row-start-4 col-start-1"
             onClick={handleBackspace}
           >
             <IoIosBackspace size={24} />
           </button>
-          <button
-            className="border text-center py-2 font-medium"
-            onClick={() => handleNumberPress(0)}
-          >
-            0
-          </button>
+
           <button
             className="border text-center py-2 font-medium"
             onClick={handleSubmit}
