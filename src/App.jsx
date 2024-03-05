faea5c0fb0a12976a95a815db54a3475605179d9
Pranjul Agrawal <pranjulagrawal9@gmail.com>
2024-03-05 17:07:01 +0530

    create UI for PIN

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import "./App.css";
 
 function App() {
-  return <>hello</>;
+  return (
+    <div className="w-96">
+      <div className="flex flex-col items-center gap-3">
+        <h2 className="uppercase font-medium">Enter 4-digit upi pin</h2>
+        <div className="flex gap-3">
+          {Array(4)
+            .fill()
+            .map((_, i) => (
+              <div
+                key={i}
+                className="w-3 h-3 border border-black rounded-full"
+              ></div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
