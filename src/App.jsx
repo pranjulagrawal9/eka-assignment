c094a99f2375e4e2c3baf9d92864ee4331a5fa6f
Pranjul Agrawal <pranjulagrawal9@gmail.com>
2024-03-05 17:23:04 +0530

    create UI for keyboard

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,19 @@ function App() {
             ))}
         </div>
       </div>
+
+      <div className="grid grid-cols-3">
+        {Array(9)
+          .fill()
+          .map((_, i) => (
+            <button key={i + 1} className="border text-center py-2">
+              {i + 1}
+            </button>
+          ))}
+        <button className="border text-center py-2">❌</button>
+        <button className="border text-center py-2">0</button>
+        <button className="border text-center py-2 font-medium">Submit</button>
+      </div>
     </div>
   );
 }
